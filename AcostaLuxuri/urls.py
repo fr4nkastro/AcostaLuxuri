0f393a07b107ed9a5bd3887be3b47a97837ae210
itIPESAH <it@ipesah.com>
2024-08-22 05:22:25 +0000

    add favorites

--- AcostaLuxuri/urls.py
+++ AcostaLuxuri/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for AcostaLuxuri project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -33,4 +17,6 @@ urlpatterns = [
     path('accesorio/remove/', views.remove_accesorio, name='create_accesorio'),
     path('accesorio/delete', views.delete_accesorio, name='insert_accesorio'),
 
+    # Nueva URL para añadir a favoritos
+    path('accesorio/favoritos', views.insert_favoritos, name='insert_favoritos'),
 ]
--- AcostaLuxuri/views.py
+++ AcostaLuxuri/views.py
@@ -1,11 +1,11 @@
 import _imp
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.shortcuts import redirect
-from lib.userLib  import LibUser
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from lib.userLib import LibUser
 from lib.accesorio import Accesorio
 from lib.libAccesorio import LibAccesorio
-
+from lib.libFavorito import LibFavorito
 def index(request): 
     return render(request,"index.html")
 
@@ -24,7 +24,7 @@ def insert_accesorio(request):
     descripcion = request.POST['descripcion']
     precio = request.POST['precio']
     libAccesorio = LibAccesorio()
-    resp = libAccesorio.create(codigo,nombre,descripcion,precio)
+    resp = libAccesorio.create(codigo, nombre, descripcion, precio)
     if(resp):
         return redirect("/home")
     else:
@@ -34,7 +34,7 @@ def edit_accesorio(request):
     codigo = request.POST['codigo']
     libAccesorio = LibAccesorio()
     accesorio = libAccesorio.get_accesorios_by_codigo(codigo)
-    return render(request,"edit.html",{'accesorio':accesorio})
+    return render(request, "edit.html", {'accesorio': accesorio})
 
 def update_accesorio(request):
     codigo = request.POST['codigo']
@@ -42,17 +42,17 @@ def update_accesorio(request):
     descripcion = request.POST['descripcion']
     precio = request.POST['precio']
     libAccesorio = LibAccesorio()
-    resp = libAccesorio.edit(codigo,nombre,descripcion,precio)
+    resp = libAccesorio.edit(codigo, nombre, descripcion, precio)
     if(resp):
         return redirect("/home")
     else:
-        return redirect("/accesorio/edit?codigo="+codigo)
+        return redirect("/accesorio/edit?codigo=" + codigo)
     
 def remove_accesorio(request):
     codigo = request.GET['codigo']
     libAccesorio = LibAccesorio()
     accesorio = libAccesorio.get_accesorios_by_codigo(codigo)
-    return render(request,"remove.html",{'accesorio':accesorio})
+    return render(request, "remove.html", {'accesorio': accesorio})
 
 def delete_accesorio(request):
     codigo = request.POST['codigo']
@@ -61,7 +61,7 @@ def delete_accesorio(request):
     if(resp):
         return redirect("/home")
     else:
-        return redirect("/accesorio/remove?codigo="+codigo)
+        return redirect("/accesorio/remove?codigo=" + codigo)
 
 def iniciar(request):
     libUser = LibUser()
@@ -69,7 +69,6 @@ def iniciar(request):
     password = request.POST['password']
     resp = libUser.login(email, password)
     if(resp):
-
         return redirect("/home")
     else: 
         return redirect("/login")
@@ -77,13 +76,16 @@ def iniciar(request):
 def home(request):
     libAccesorio = LibAccesorio()
     lista = libAccesorio.get_accesorios()
+    return render(request, "home.html", {"lista": lista})
 
-    return render(request,"home.html",{"lista":lista})
-
-#def mostrar_accesorios(request):
-    # Leer el archivo JSON
-    #file = open('data/productos.json', 'r')
-    #data = json.load(file)
-    
-    # Pasar los datos a la plantilla
-    #return render(request, 'index.html', {'accesorios': data['accesorios,']})
+def insert_favoritos(request):
+    if request.method == 'POST':
+        codigo = request.POST.get('codigo')
+        libFavorito = LibFavorito()
+        resp = libFavorito.create(codigo=codigo)
+        if(resp):
+            messages.success(request, 'Accesorio añadido a favoritos.')
+            return redirect("/home")
+        else:
+            messages.info(request, 'Accesorio no añadido a favoritos.')
+    return redirect('home')
